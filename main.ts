251bf903281b763ee045b1a7a1469c06a4e07ea6
naim <96814060+naim514@users.noreply.github.com>
2022-04-25 21:25:32 +0200

    Mise à jour pxt.json, main.blocks, main.ts, hello-iot.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    ESP8266ThingSpeak.connectThingSpeak(
+    "api.thingspeak.com",
+    "your_write_api_key",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+    )
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString(PTKidsBITIoT.HTTPGet("", 443))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("" + (PTKidsBITIoT.GETFromJson("", "")))
+})
+ESP8266ThingSpeak.connectWifi(
+SerialPin.P8,
+SerialPin.P12,
+BaudRate.BaudRate115200,
+"your_ssid",
+"your_pw"
+)
